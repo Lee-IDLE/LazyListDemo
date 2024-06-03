49f4ecbaa78e33b53a24fa8dd1e74ba9307103ea
Lee-IDLE <nidus0526@gmail.com>
2024-06-03 11:01:28 +0900

    1. 스티키 헤더 추가(제조사별 그룹) 2. 마지막으로 스크롤시 화면 위로 올려주는 기능 버튼 추가

--- app/src/main/java/com/lee_idle/lazylistdemo/MainActivity.kt
+++ app/src/main/java/com/lee_idle/lazylistdemo/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,15 +21,22 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,6 +45,7 @@ import coil.annotation.ExperimentalCoilApi
 import coil.compose.ImagePainter
 import coil.compose.rememberImagePainter
 import com.lee_idle.lazylistdemo.ui.theme.LazyListDemoTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private var itemArray: Array<String>? = null
@@ -53,11 +67,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen(itemArray: Array<out String>) {
     val context = LocalContext.current
+    val groupedItem = itemArray.groupBy { it.substringBefore(' ') }
 
-    val onLisdtItemClick = { text: String ->
+    val onListItemClick = { text: String ->
         Toast.makeText(
             context,
             text,
@@ -65,9 +81,76 @@ fun MainScreen(itemArray: Array<out String>) {
         ).show()
     }
 
-    LazyColumn{
-        items(itemArray) { model ->
-            MyListItem(item = model, onItemClick = onLisdtItemClick)
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val displayButton = listState.firstVisibleItemIndex > 5
+
+    Box {
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(bottom = 5.dp)
+        ){
+            groupedItem.forEach{ (menufacturer, models) ->
+                stickyHeader {
+                    Text(
+                        text = menufacturer,
+                        color = Color.White,
+                        modifier =
+                        Modifier
+                            .background(Color.Gray)
+                            .padding(5.dp)
+                            .fillMaxWidth()
+                    )
+                }
+
+                items(models) { model ->
+                    MyListItem(item = model, onItemClick = onListItemClick)
+                }
+            }
+        }
+
+        /*
+        // 스크롤이 아래로 갔을 때 애니메이션 효과를 적용해 버튼을 보이도록 한다
+        AnimatedVisibility(visible = displayButton,
+            modifier = Modifier.align(Alignment.BottomEnd)) {
+            // 스크롤을 위로 올려줄 버튼
+            OutlinedButton(
+                onClick = {
+                    coroutineScope.launch {
+                        listState.scrollToItem(0)
+                    }
+                },
+                border = BorderStroke(1.dp, Color.Gray),
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = Color.DarkGray
+                ),
+                modifier = Modifier.padding(5.dp)
+            ) {
+                Text(text = "Top")
+            }
+        }
+         */
+
+        // 바로 위에 있는 버튼을 따로 표시가 아닌, LazyColumn과 겹쳐서 보이도록 수정한 코드
+        // outline 버튼에서 fill 버튼으로 교체
+        AnimatedVisibility(visible = displayButton,
+            modifier = Modifier.align(Alignment.BottomEnd)) {
+            FilledTonalButton(
+                onClick = {
+                    coroutineScope.launch {
+                        listState.scrollToItem(0)
+                    }
+                },
+                border = BorderStroke(1.dp, Color.Gray),
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.filledTonalButtonColors(
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.padding(bottom = 10.dp, end = 5.dp).alpha(0.5f)
+            ) {
+                Text(text = "Top")
+            }
         }
     }
 }
